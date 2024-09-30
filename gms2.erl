12b3734d5a3aad813a5f6fa2d35b97188ab0b034
sofieschn <sofie.schnitzer@gmail.com>
2024-09-30 11:18:44 +0200

    gms2 created

---
+++ gms2.erl
@@ -0,0 +1,156 @@
+% group membership service 1
+-module(gms2).
+-export([leader/4, broadcast/3, slave/5, start/1, init/2, start/2, init/3]).
+
+%%%%% LEADER NODE START AND INIT %%%%%
+
+%%% Starting the first node (leader) 
+%%% Starting the first node (leader) %%%
+start(Id) ->
+    Self = self(),
+    {ok, spawn_link(fun() -> init(Id, Self) end)}.
+
+%%% Initialization for the first node %%%
+init(Id, Master) ->
+    % The first node automatically becomes the leader
+    leader(Id, Master, [], [Master]).
+
+
+%%%%% JOINING NODE START AND INIT %%%%%
+
+%%% Starting a node that joins an existing group (slave) %%%
+start(Id, Grp) ->
+    %%Self = self(),
+    % Spawn a new application layer (Master) process for each joining node
+    Master = spawn(fun() -> application_process() end),
+    {ok, spawn_link(fun() -> init(Id, Grp, Master) end)}.
+
+%% starts an application process for each started node. They all get unique processIDs
+application_process() ->
+    receive
+        % Handle messages from the group process
+        Message ->
+            application_process()
+    end.
+
+%%% Initialization for a joining node %%%
+init(Id, Grp, Master) ->
+    Self = self(),
+    
+    % Send a join request to any existing node in the group
+    Grp ! {join, Master, Self},
+    % Wait for the view message from the leader
+    receive
+        {view, [Leader | NewSlavesList], NewGroup} ->
+            io:format("Slave ~p received new view: Leader: ~p, Slaves: ~p, Group: ~p~n",[Id, Leader, NewSlavesList, NewGroup]),  % Print new group view
+            % Inform the application layer of the updated view
+            Master ! {view, NewGroup},
+            %% The only node that will be monitored is the leader
+			erlang:monitor(process, Leader),
+            % Start the slave process with the received leader, slaves, and group
+            slave(Id, Master, Leader, NewSlavesList, NewGroup)
+
+    % create a timeout if there is no connection to the leader. To not leave it waiting forever.
+	after 5000 -> 
+		%% send an error message to the master when a reply is not received
+        io:format("Node ~p did not receive a reply from the leader in time. Aborting join request.~n", [Id]),
+		Master ! {error, "no reply from leader"}
+	end.
+
+leader(Id, Master, Slaves, Group) ->
+    receive
+        % sending message to entire group
+        {mcast, Message} ->
+            % broadcast message to all slaves. 
+            % message is wrapped in a tuple to tag it with 'msg' as a label to help differentiate message from other messages such as add etc. 
+            broadcast(Id, {msg, Message}, Slaves),
+            % Send message to leaders own application process (Master)
+            Master ! Message,
+            % recursively call leader function with updated state
+            leader(Id, Master, Slaves, Group);
+
+        % new node join handler
+        {join, NewWork, NewSlave} ->
+            % Add the new slave to the list of slaves (end of list)
+            NewSlavesList = lists:append(Slaves, [NewSlave]),
+            % Add the new application process to Group 
+            NewGroup = lists:append(Group, [NewWork]),
+            % broadcast the new slave of the group to the slaves, the leader includes itself in the new list of slaves too
+            broadcast(Id, {view, [self()| NewSlavesList], NewGroup}, NewSlavesList),
+            % update its own view by "broadcasting" to itself aswell
+            Master ! {view, NewGroup},
+            % recursively call the leader function to keep waiting for new updates
+            leader(Id, Master, NewSlavesList, NewGroup);
+        stop ->
+            ok
+
+    end. 
+
+% Base case, end of slaves list
+broadcast(_Id, _Message, []) ->
+    ok;
+% Recursive case, broadcasting to each slave
+broadcast(Id, Message, [Slave|Rest]) ->
+    io:format("Broadcasting message ~p to slave ~p~n", [Message, Slave]),  % Log broadcast messages
+    Slave ! Message,
+    % keep calling broadcast with the rest of the list
+    broadcast(Id, Message, Rest).
+
+
+slave(Id, Master, Leader, Slaves, Group) ->
+
+    receive
+        % Handle multicast message from the application layer or another node
+        {mcast, Message} ->
+            io:format("Slave ~p forwarding multicast message to leader~n", [Id]),  % Log when forwarding multicast
+            % Forward the multicast message to the Leader
+            Leader ! {mcast, Message},
+            % Continue the slave loop with the same state
+            slave(Id, Master, Leader, Slaves, Group);
+
+        % Handle a join request for a new node
+        {join, Work, Peer} ->
+            io:format("Slave ~p forwarding join request to leader~n", [Id]),  % Log when forwarding join request
+            % Forward the join request to the Leader
+            Leader ! {join, Work, Peer},
+            % Continue the slave loop with the same state
+            slave(Id, Master, Leader, Slaves, Group);
+
+        % Handle a regular message (from the leader)
+        {msg, Message} ->
+            io:format("Slave ~p received message from leader: ~p~n", [Id, Message]),  % Log when receiving message from leader
+            % Forward the message to the application layer (Master)
+            Master ! Message,
+            % Continue the slave loop with the same state
+            slave(Id, Master, Leader, Slaves, Group);
+
+        % Handle a new view update (when group membership changes)
+        {view, [Leader|NewSlavesList], NewGroup} ->
+            io:format("Slave ~p received new view: Leader: ~p, Slaves: ~p, Group: ~p~n",[Id, Leader, NewSlavesList, NewGroup]),  % Log new group view
+            % Send the new view of the group to the application layer (Master)
+            Master ! {view, NewGroup},
+            % Update the slave's local state with the new Leader, Slaves list, and Group
+            slave(Id, Master, Leader, NewSlavesList, NewGroup);
+
+        {'DOWN', _Ref, process, Leader, _Reason} ->
+                election(Id, Master, Slaves, Group);
+
+        % Handle the stop signal (terminates the slave process)
+        stop ->
+            ok
+    end.
+
+%% elects a new leader when the leader node has 'gone down'
+election(Id, Master, Slaves, [_|Group]) ->
+    Self = self(),
+    case Slaves of
+        [Self|Rest] ->
+            broadcast(Id, {view, Slaves, Group}, Rest),
+            Master ! {view, Group},
+            leader(Id, Master, Rest, Group);
+        [Leader|Rest] ->
+            erlang:monitor(process, Leader),
+            slave(Id, Master, Leader, Rest, Group)
+    end.
+
+
